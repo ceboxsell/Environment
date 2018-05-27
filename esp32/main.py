c2afdaba1418b9ae46a786c033a545e87e9b81a1
Chris Boxsell <ceboxsell@users.noreply.github.com>
2018-05-27 13:35:55 +1000

    Test deep sleep
    testing deep sleep  failed first go

--- esp32/main.py
+++ esp32/main.py
@@ -1,12 +1,12 @@
-
-
-
 import time
 import ubinascii
 import micropython
 import machine
 from machine import Pin
 import dht
+
+import machine as m
+
 
 from uqmtt.robust import MQTTClient
 
@@ -16,15 +16,18 @@ ConnectWifi.connect()
 
 d = dht.DHT22(machine.Pin(16))
 
-SERVER = "192.168.86.232"
+SERVER = "192.168.1.6"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
 KEEPALIVE = 60
-TOPIC = b"temp"
-LED_PIN = 2
-TOUCH_PIN = 15
+TOPIC = "temperature"
+#sleep time in seconds
+DEEPSLEEP = "300"
+#LED_PIN = 2
+#TOUCH_PIN = 15
+
 #LED = "blue"
-led_flashing = 0
-led_status = 0
+#led_flashing = 0
+#led_status = 0
 connected = False
 
 d.measure()
@@ -33,17 +36,11 @@ humidity = d.humidity()
 print('Hum ' + str(humidity) + ' Temp ' + str(temp))
 
 
-def settimeout(duration): 
-    pass
+#def settimeout(duration): 
+#    pass
 
-#def short_blink():
-#    for i in range(4):
-#        led.value(1)
-#        time.sleep_ms(100)
-#        led.value(0)
-#        time.sleep_ms(100)
 
-def sub_cb(topic, msg):
+#def sub_cb(topic, msg):
 #    global led_flashing
 #    print((topic, msg))
     #msg = json.loads(msg)
@@ -57,21 +54,19 @@ def sub_cb(topic, msg):
         #led.value(0)
 #        temp = d.temperature()
 #        humidity = d.humidity()
-        client.publish(topic=b"temp", msg=str(temp))
-        client.publish(topic=b"humidity", msg=str(humidity))
+#        client.publish(topic=b"temperature/temp", msg=str(temp))
+#        client.publish(topic=b"temperature/humidity", msg=str(humidity))
 #    elif msg != b"on":
 #        led_flashing = 0
 
-led = machine.Pin(LED_PIN, machine.Pin.OUT, value=1)
-touch = machine.TouchPad(machine.Pin(TOUCH_PIN))
 
 client = MQTTClient(CLIENT_ID, SERVER, keepalive = KEEPALIVE)
 client.DEBUG = True
-client.settimeout = settimeout
-client.set_callback(sub_cb)
+#client.settimeout = settimeout
+#client.set_callback(sub_cb)
 #client.connect()
 #client.subscribe(TOPIC, qos=1)
-print("Connected to %s, subscribed to %s topic" % (SERVER, TOPIC))
+#print("Connected to %s, subscribed to %s topic" % (SERVER, TOPIC))
 
 try:
     while 1:
@@ -85,26 +80,29 @@ try:
             last_publish = 0
             last_ping = time.ticks_ms()
 
-            client.subscribe(TOPIC, qos=1)
-            client.publish(topic=b"temp", msg=str(temp))
-            client.publish(topic=b"humidity", msg=str(humidity))
+            #client.subscribe(TOPIC, qos=1)
+            client.publish(topic=b"temperatire/temp", msg=str(temp))
+            client.publish(topic=b"temperature/humidity", msg=str(humidity))
             #client.publish(topic=b"led", msg="off")
+            client.disconnect()
+            connected = False
+            m.deepsleep(sleep_ms = DEEPSLEEP)
+            
         else:
 
             # Periodically ping the broker consistently with the "keepalive"
             # argument used when connecting.  If this isn't done, the broker will
             # disconnect when the client has been idle for 1.5x the keepalive
             # time.
-            if time.ticks_ms() - last_ping >= KEEPALIVE * 1000:
-                 client.ping()
-                 last_ping = time.ticks_ms()
-            time.sleep(4)
             d.measure()
             temp = d.temperature()
             humidity = d.humidity()
             print('Hum ' + str(humidity) + ' Temp ' + str(temp))
-            client.publish(topic=b"temp", msg=str(temp))
-            client.publish(topic=b"humidity", msg=str(humidity))
+            client.publish(topic=b"temperature/temp", msg=str(temp))
+            client.publish(topic=b"temperature/humidity", msg=str(humidity))
+            client.disconnect()
+            connected = False
+            m.deepsleep(sleep_ms = DEEPSLEEP)
 
         # turn off LED with touch
         #if led_flashing == 1 and touch.read() < 1200:
@@ -119,6 +117,7 @@ try:
         #    led.value(0)
         #    led_status = 0
         #    time.sleep(0.1)
+
         #elif led_flashing == 0 and led_status == 1:
          #   led.value(0)
         #else:
@@ -134,6 +133,5 @@ try:
                 continue
             
 finally:
+
     client.disconnect()
-
-
